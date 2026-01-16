587d068899a90e04c250e050e39360a4d4ab14c6
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-16 15:52:01 +0100

    Add Contact and ContactStatus models with fields

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ContactStatus(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=150)
+    phone_number = models.CharField(20, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    city = models.CharField(max_length=60)
+    status = models.ForeignKey(ContactStatus, on_delete=models.PROTECT)
+    created = models.DateTimeField(auto_now_add=True)
