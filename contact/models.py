9b83f7d04ed3137aac93a5e33ab39123c855a985
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-16 16:00:10 +0100

    fix modelu Contact: added method __str__ and fixed phone_number field definition

--- contact/models.py
+++ contact/models.py
@@ -13,8 +13,11 @@ class ContactStatus(models.Model):
 class Contact(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=150)
-    phone_number = models.CharField(20, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
     email = models.EmailField(max_length=254, unique=True)
     city = models.CharField(max_length=60)
     status = models.ForeignKey(ContactStatus, on_delete=models.PROTECT)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
