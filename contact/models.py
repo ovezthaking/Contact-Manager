f91da54ddcb9477947d70c51306991a6749353ff
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-17 03:09:33 +0100

    Refactor Contact model: add phone number validation and update email field definition

--- contact/models.py
+++ contact/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import EmailValidator, RegexValidator
 
 # Create your models here.
 
@@ -11,10 +12,24 @@ class ContactStatus(models.Model):
 
 
 class Contact(models.Model):
+    # formaty typu: +48123456789, 123-456-789
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}',
+        message='Phone format: +99999999. Up to 15 digits.'
+    )
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=150)
-    phone_number = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(max_length=254, unique=True)
+    phone_number = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[phone_regex]
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        validators=[EmailValidator()]
+    )
     city = models.CharField(max_length=60)
     status = models.ForeignKey(ContactStatus, on_delete=models.PROTECT)
     created = models.DateTimeField(auto_now_add=True)
