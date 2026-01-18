44a86b8655a5fbd7f7fc76d630390a6165659b34
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-18 18:13:01 +0100

    Add createContact view and form template for new contact submissions

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('contact/<str:pk>/', views.contact, name='contact'),
+    path('create-contact/', views.createContact, name='create-contact')
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -16,3 +17,16 @@ def contact(request, pk):
     contact = Contact.objects.get(id=pk)
     context = {'contact': contact}
     return render(request, 'contact/contact.html', context)
+
+
+def createContact(request):
+    form = ContactForm()
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'contact/contact_form.html', context)
